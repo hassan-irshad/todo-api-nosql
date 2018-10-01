c051b368be0b3f919f87f87a7414cb7aadb68dc2
Hassan Irshad <hassanansari17@live.com>
2018-10-02 01:09:09 +0500

    Adding the test case for GET /todo/api/v1.0/tasks

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,15 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
+var testTodo = {
+    title: 'Test',
+    description: 'It is for test'
+}
+beforeEach((done) => {
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(testTodo);
+    }).then(() => done());
+});
 
 describe('POST /todo/api/v1.0/tasks', () => {
     it('should create a new task', (done) => {
@@ -18,6 +27,18 @@ describe('POST /todo/api/v1.0/tasks', () => {
             .expect(200)
             .expect((res) => {
                 expect(res.body.title).toBe(todo.title);
+            })
+            .end(done);
+    });
+});
+
+describe('GET /todo/api/v1.0/tasks', () => {
+    it('should get all the tasks', (done) => {
+        request(app)
+            .get('/todo/api/v1.0/tasks')
+            .status(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(1);
             })
             .end(done);
     });
